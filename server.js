d4b609e6013ef97d02c0f4eb0fd974319e6940fb
hansexebio <hansexebio@gmail.com>
2022-10-18 21:45:51 -0500

    sockets.io

--- server.js
+++ server.js
@@ -1,12 +1,28 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
 const port = 3000
+const io = new Server(server, {  cors: {  origin: "*",    methods: ["GET", "POST"]  }});
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-  console.log('Conected!')
-})
+  res.sendFile(__dirname + '/index.html');
+});
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+io.on('connection', (socket) => {
+  console.log('nueva conexion')
+  socket.broadcast.emit('chat message', 'Usuario Nuevo!! Dale la bienvenida a ' + socket.id.toString());
+  const id_handshake = socket.id;
+  console.log(id_handshake);
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+  socket.on('disconnect', () => {
+    console.log('usuario desconectado');
+  });
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:' + port.toString());
+});
